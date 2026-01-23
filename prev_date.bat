981fd791287f930b04ecd9a56ab4a639e4e4d41b
nnnaanaa <53326220+nnnaanaa@users.noreply.github.com>
2026-01-23 19:54:38 +0900

    add debug py

--- prev_date.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-
-rem 前日の年月日を YYYYMMDD 形式で取得するバッチファイル
-set TARGET_DATE=20260119
-for /f %%a in ('powershell -command "([datetime]('%TARGET_DATE%'.Insert(6,'/').Insert(4,'/'))).AddDays(-1).ToString('yyyyMMdd')"') do ( set YESTERDAY=%%a )
-echo %YESTERDAY%
